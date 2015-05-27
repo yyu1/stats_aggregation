31f51bdc8b989a92e1efc154458865bc7e1c6b49
Yifan Yu <yifany@ucla.edu>
2015-05-26 17:39:03 -0700

    bug fixes

--- main_image.cc
+++ main_image.cc
@@ -1,4 +1,4 @@
-#define IMAGE_T unsigned int
+#define IMAGE_T short
 
 #include <iostream>
 #include <fstream>
@@ -15,15 +15,18 @@ int main()
 	const unsigned long xdim_pixels = 172800;
 	const unsigned long ydim_pixels = 86400;
 
-	const unsigned int nrows_per_deg = xdim_pixels/latitude_span;
+	const unsigned int nrows_per_deg = ydim_pixels/latitude_span;
 	const double pixel_size = 0.00208333333333333;
 
-	const std:string in_file("/Volumes/wd_3t/maxent/250m/v4.2/biomass/maxent_agb_global_vcf5mask_v4.2.5_wd3_latlon.int");
-	const std:string out_file("/Volumes/Global_250m/stats/maxent_agb_v4.2.5_wd3_1deg_stats.txt");
+	const std::string in_file("/Volumes/wd_3t/maxent/250m/v4.2/biomass/maxent_agb_global_vcf5mask_v4.2.5_wd3_latlon.int");
+	const std::string out_file("/Volumes/Global_250m/stats/maxent_agb_v4.2.5_wd3_1deg_stats.txt");
 
 	//---------------end config
 
-	IMAGE_T input_buffer[nrows_per_deg][xdim_pixels];
+	IMAGE_T **input_buffer = new IMAGE_T *[nrows_per_deg];
+	for (int i=0; i<nrows_per_deg; i++) {
+		input_buffer[i] = new IMAGE_T[xdim_pixels];
+	}
 	
 
 	GlobalStatsGrid *global_grid = new GlobalStatsGrid();
@@ -38,17 +41,21 @@ int main()
 
 	for (int latitude=0; latitude < latitude_span; latitude++) {
 		//read the input block 1 row at a time into buffer
+		std::cout << "latitude: " << latitude << std::endl;  //debug
 		for (int i=0; i<nrows_per_deg; i++) {
-			in_file.read((char *) input_buffer[latitude], xdim_pixels);
+			unsigned long bytes_to_read = xdim_pixels * sizeof(IMAGE_T);
+			in_file_stream.read((char *) input_buffer[i], bytes_to_read);
 		}
 
 		//cycle through buffer and add >0 AGB values (can't use non-zero since -1 can be used for special purposes like water)
 		for (int i=0; i<xdim_pixels; i++) {
-			longitude = (unsigned int) (pixel_size * (double)xdim_pixels);
+			longitude = (int) (pixel_size * (double)xdim_pixels);
 			if (longitude == 360) longitude = 359; //fix edge of last pixel
 			for (int j=0; j<nrows_per_deg; j++) {
 				if (input_buffer[j][i] > 0) {
-					global_grid->addValue(((float)input_buffer[j][i]/10), (float)longitude, (float)latitude);
+					//std::cout << "AGB at " << i << " " << j << " :" << input_buffer[j][i] << " " << "latitude " << latitude << std::endl;   //debug
+					//return 0;   //debug
+					global_grid->addValue(((float)input_buffer[j][i]/10), (float)(longitude-180), (float)(90-latitude));
 				}
 			}
 		}
@@ -56,6 +63,9 @@ int main()
 
 	//finished processing input file.  Close stream
 	in_file_stream.close();
+	for (int j=0; j<nrows_per_deg; j++) {
+		delete [] input_buffer[j];
+	}
 	
 
 	std::ofstream out_file_stream;
